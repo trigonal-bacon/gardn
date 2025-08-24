9792b5480179f440815f904596c6c9c8415e7e54
h <jerry.h.song@gmail.com>
2025-08-24 19:31:25 -0400

    fix

--- Client/Render/RenderPetal.cc
+++ Client/Render/RenderPetal.cc
@@ -16,6 +16,6 @@
 void render_petal(Renderer &ctx, Entity const &ent) {
     ctx.scale(ent.radius / PETAL_DATA[ent.petal_id].radius);
     draw_static_petal_single(ent.petal_id, ctx);
-    if (PETAL_DATA[ent.petal_id].rarity == RarityID::kUnique && frand() < fclamp(powf(0.25, (Ui::dt/16.67)), 0, 1))
+    if (PETAL_DATA[ent.petal_id].rarity == RarityID::kUnique && frand() < fclamp(0.25 * Ui::dt/16.67, 0, 1))
         Particle::add_unique_particle(ent.x, ent.y);
 }
